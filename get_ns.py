f4a5d0e709f0e8b085f3d465171d95945888f791
Tom Ritchford <tom@swirly.com>
2017-09-29 23:19:21 +0200

    Final tweak

--- get_ns.py
+++ get_ns.py
@@ -3,9 +3,7 @@ import os
 path = "/net/u/1/j/jongiles/scripts/domains/names"
 
 TEST_STRINGS = "Name server", "name server", "Name Server"
-
 active_domains = []
-inactive_domains = []
 
 def is_active_line(line):
     """Return true if the line of the file indicates that the domain is active.
@@ -13,19 +11,13 @@ def is_active_line(line):
     return any(ts in line for ts in TEST_STRINGS)
 
 
-def is_active_file(path, item):
-    fp = open(os.path.join(path, item))
-    return any(is_active_file(line) for line in fp)
+def is_active_domain(f):
+    return any(is_active_file(line) for line in open(f))
 
 
-domainfiles = [f for f in os.listdir(path)
-               if os.path.isfile(os.path.join(path, f))]
-
-for item in domainfiles:
-    print(item)
-
-    if not if_active_file(path, item):
-        inactive_domains.append(item)
+directory = [os.path.join(path, f) for f in os.listdir(path)]
+domainfiles = [f for f in directory if os.path.isfile(f)]
+inactive_domains = [f for f in domainfiles if not is_active_domain(f)]
 
 print("This is a list of inactive domains")
 print(inactive_domains)
