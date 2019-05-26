c14f272b0113bcd6f04325b5769a819a96ed103f
Tom Ritchford <tom@swirly.com>
2019-05-26 11:42:45 +0200

    Simplest example for strange import

---
+++ strange_import/main.py
@@ -0,0 +1,38 @@
+import importlib
+import sys
+
+
+def _import(module):
+    sys_modules = dict(sys.modules)
+    importlib.import_module(module)
+    sys.modules = sys_modules
+
+
+FAILURE = True
+if FAILURE:
+    _import('extract')
+
+_import('extract.one')
+_import('extract.two')
+
+"""
+If FAILURE if False, the program completes.
+If FAILURE is True, it throws this exception.
+
+Traceback (most recent call last):
+  File "main.py", line 16, in <module>
+    _import('extract.two')
+  File "main.py", line 7, in _import
+    importlib.import_module(module)
+  File "/code/env/nc/lib/python3.6/importlib/__init__.py", line 126, in import_module
+    return _bootstrap._gcd_import(name[level:], package, level)
+  File "<frozen importlib._bootstrap>", line 994, in _gcd_import
+  File "<frozen importlib._bootstrap>", line 971, in _find_and_load
+  File "<frozen importlib._bootstrap>", line 955, in _find_and_load_unlocked
+  File "<frozen importlib._bootstrap>", line 665, in _load_unlocked
+  File "<frozen importlib._bootstrap_external>", line 678, in exec_module
+  File "<frozen importlib._bootstrap>", line 219, in _call_with_frames_removed
+  File "/code/test/relative_import/extract/two.py", line 1, in <module>
+    from . import one
+ImportError: cannot import name 'one'
+"""
