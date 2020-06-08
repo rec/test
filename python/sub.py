59e94fb10274e16bc7ceeddfe732d68cae41caf5
Tom Ritchford <tom@swirly.com>
2020-06-08 14:32:46 +0200

    Modify python/sub.py

--- python/sub.py
+++ python/sub.py
@@ -10,18 +10,27 @@ async def run():
     async def read(stream):
         message = 'E' if stream is proc.stderr else 'O'
         while True:
+            print(message, '+')
             line = await stream.readline()
+            print(message, '-')
             if line:
                 print(message, line)
             else:
                 break
 
     async def write():
-        for command in (b'PS1="hello\n"', b'ls sub.py', b'ls DOESNT-EXIST'):
+        # COMMANDS = b'PS1="hello\n"', b'ls sub.py', b'ls DOESNT-EXIST'
+        COMMANDS = b'PS1="hello\n"', b'ls sub.py', b'ls DOESNT-EXIST', b'exit'
+        for command in COMMANDS:
             proc.stdin.write(command + b'\n')
             await proc.stdin.drain()
-            await asyncio.sleep(0.01)
-        proc.terminate()
+            await asyncio.sleep(0.2)
+
+        print('terminate')
+        # proc.kill()
+        print('terminated')
+        print('proc', dir(proc))
+        print('proc.stdout', dir(proc.stdout))
 
     await asyncio.gather(
         read(proc.stderr),
