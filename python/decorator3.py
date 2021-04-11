83ed4dbc6742d39e4ae22d1ba1b3fe08f55e3d29
Tom Ritchford <tom@swirly.com>
2021-04-11 21:36:57 +0200

    Add python/decorator3.py

---
+++ python/decorator3.py
@@ -0,0 +1,32 @@
+from functools import wraps, partial
+
+class Class:
+    pass
+
+ac = Class()
+
+def mock_input_decorator(func=None, **options):
+    if func == None:  # Better - if func is None
+        return partial(mock_input_decorator, **options)
+
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        def wrapper(s="p"):
+            return options.pop()
+        ac.input = wrapper
+        return func(*args, **kwargs)
+    return wrapped
+
+
+# @mock_input_decorator(["1"])  # this wouldn't work.
+@mock_input_decorator(choice=["1"])
+def test_get_input():
+    print('inside!')
+
+@mock_input_decorator
+def test_get_input2():
+    print('inside 2!')
+
+
+print(test_get_input())
+print(test_get_input2())
