459ec283f0a341704e7fc11f9ccdbe3d6ab5b469
Tom Ritchford <tom@swirly.com>
2022-03-01 18:05:33 +0100

    [split] Add python/long_assertions.py

---
+++ python/long_assertions.py
@@ -0,0 +1,3 @@
+    a_very_very_very_very_very_very_very_very_very_very_very_long_variable = 1
+
+    assert a_very_very_very_very_very_very_very_very_very_very_very_long_variable == a_very_very_very_very_very_very_very_very_very_very_very_long_variable, 'a_very_very_very_very_very_very_very_very_very_very_very_long_message'
