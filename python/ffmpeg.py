1eed3f35559b51b2934ee06b94a37ee3fd4cac8d
Tom Ritchford <tom@swirly.com>
2022-12-13 13:28:29 +0100

    Modify python/ffmpeg.py

--- python/ffmpeg.py
+++ python/ffmpeg.py
@@ -9,33 +9,35 @@ SRC = Path('/Users/tom/Downloads/Dead.to.Me.S03.COMPLETE.1080p.NF.WEB.H264-MIXED
 TARGET = Path('/Users/tom/Downloads/dead')
 
 
-def main(src=SRC, target=TARGET):
-    for s in sorted(s for s in src.iterdir() if s.is_dir()):
-        for f in sorted(t for t in s.iterdir() if t.suffix == '.mkv'):
-            tf = target / f.relative_to(src).with_suffix('.mp4')
-            if tf.exists():
-                continue
+def main(src, target, bv='4800'):
+    src, target = Path(src), Path(target)
+    assert src.exists(), str(src)
+    for f in sorted(t for t in src.iterdir() if t.suffix in ('.mkv', '.mp4')):
+        tf = target / f.relative_to(src).with_suffix('.mp4')
+        if False and tf.exists():
+            continue
 
-            tf.parent.mkdir(parents=True, exist_ok=True)
-            cmd = (
-                'ffmpeg',
-                '-i', str(f),
-                '-c:v', 'h264_videotoolbox',
-                '-b:v', '4800k',
-                str(tf)
-            )
-            print('$', *cmd)
-            if not True:
-                continue
+        tf.parent.mkdir(parents=True, exist_ok=True)
+        cmd = (
+            'ffmpeg',
+            '-i', str(f),
+            '-c:v', 'h264_videotoolbox',
+            '-b:v', f'{bv}k',
+            str(tf)
+        )
+        print('$', *cmd)
+        if not True:
+            continue
 
-            try:
-                finished = False
-                subprocess.run(cmd)
-                finished = True
-            finally:
-                if not finished:
-                    tf.unlink()
+        try:
+            finished = False
+            subprocess.run(cmd)
+            finished = True
+        finally:
+            if not finished:
+                tf.unlink()
 
 
 if __name__ == '__main__':
-    main()
+    import sys
+    main(*sys.argv[1:])
