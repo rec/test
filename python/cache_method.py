8ac2a1728eb21bc65b68999b5ced1aff02a66c23
Tom Ritchford <tom@swirly.com>
2023-01-01 18:35:22 +0100

    Modify python/cache_method.py

--- python/cache_method.py
+++ python/cache_method.py
@@ -11,12 +11,6 @@ class Class:
         return next(COUNT)
 
 
-class LruClass:
-    @functools.lru_cache(maxsize=None)
-    def count(self):
-        return next(COUNT)
-
-
 @dataclass(frozen=True)
 class Dataclass:
     @functools.cache
@@ -24,39 +18,11 @@ class Dataclass:
         return next(COUNT)
 
 
-@dataclass(frozen=True)
-class LruDataclass:
-    @functools.lru_cache(maxsize=None)
-    def count(self):
-        return next(COUNT)
-
-
-class Child(Class):
-    pass
-
-
-@dataclass(frozen=True)
-class DataChild(Class):
-    pass
-
-
-def test_ok(cls):
+def test_cache(cls):
     a, b = cls(), cls()
     print(f'{a.count()=} {b.count()=}')
     assert a.count() + 1 == b.count()
 
 
-def test_fails(cls):
-    a, b = cls(), cls()
-    print(f'{a.count()=} {b.count()=}')
-    assert a.count() == b.count()
-
-
-test_ok(Class)
-test_fails(Dataclass)
-
-test_ok(Child)
-test_fails(DataChild)
-
-test_ok(LruClass)
-test_fails(LruDataclass)
+test_cache(Class)
+test_cache(Dataclass)
