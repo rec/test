3ae8fe9f9e27967c321a4fc400d7e5081c1fa494
Tom Ritchford <tom@swirly.com>
2024-07-25 14:31:41 +0000

    Modify python/test_dict_keys.py

--- python/test_dict_keys.py
+++ python/test_dict_keys.py
@@ -5,9 +5,7 @@ from torch._dynamo.eval_frame import optimize_assert
 @torch._dynamo.optimize(nopython=True)
 def test_dict_keys(x):
     d = {3: x}
-    keys = d.keys()
-    d[4] = x + 1
     d2 = {}
-    return d2.keys() == keys
+    return d2.keys() == d.keys()
 
 test_dict_keys(torch.randn(10, 10))
