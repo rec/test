00435dd544160bc1fe6ccdc08302d76f7fb005e5
Tom Ritchford <tom@swirly.com>
2024-07-25 14:00:16 +0000

    Modify python/test_dict_keys.py

--- python/test_dict_keys.py
+++ python/test_dict_keys.py
@@ -2,15 +2,6 @@ import torch
 from torch._dynamo import testing, eval_frame
 from torch._dynamo.eval_frame import optimize_assert
 
-def standard_test(fn):
-    actual = testing.CompileCounter()
-
-    args1 = [torch.randn(10, 10)]
-    correct1 = fn(*args1)
-    opt_fn = optimize_assert(actual)(fn)
-    val1a = opt_fn(*args1)
-
-
 def test_dict_keys(x):
     d = {3: x}
     keys = d.keys()
@@ -18,4 +9,8 @@ def test_dict_keys(x):
     d2 = {3: 2, 4: "aa"}
     return 3 in keys, 4 in keys, 5 in keys, d2.keys() == keys
 
-standard_test(test_dict_keys)
+actual = testing.CompileCounter()
+
+args1 = [torch.randn(10, 10)]
+opt_fn = optimize_assert(actual)(test_dict_keys)
+val1a = opt_fn(*args1)
