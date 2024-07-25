0f8b8950a6a15e7bd945ab904af7641402b1ccd8
Tom Ritchford <tom@swirly.com>
2024-07-25 14:30:45 +0000

    Modify python/test_dict_keys.py

--- python/test_dict_keys.py
+++ python/test_dict_keys.py
@@ -7,7 +7,7 @@ def test_dict_keys(x):
     d = {3: x}
     keys = d.keys()
     d[4] = x + 1
-    d2 = {3: 2, 4: "aa"}
+    d2 = {}
     return d2.keys() == keys
 
 test_dict_keys(torch.randn(10, 10))
