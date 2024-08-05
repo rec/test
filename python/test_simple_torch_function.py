e1b1fd6b3ee4ace6b252960e3f193a59208938e0
Tom Ritchford <tom@swirly.com>
2024-08-05 14:12:13 +0000

    Modify python/test_simple_torch_function.py

--- python/test_simple_torch_function.py
+++ python/test_simple_torch_function.py
@@ -4,13 +4,7 @@ import torch.nn.functional as F
 
 @torch._dynamo.optimize(nopython=True)
 def simple_function(x):
-    # function call, twice to test wrapping
-    x = F.sigmoid(x)
-    x = F.sigmoid(x)
-    # method call, twice to test wrapping
-    x = x.sigmoid()
-    x = x.sigmoid()
-    return x
+    return F.sigmoid(x)
 
 
 class TensorProxy(torch.Tensor):
