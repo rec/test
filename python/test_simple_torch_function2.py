27f0cdad00434b64f32db5e20f1239699f51aa92
Tom Ritchford <tom@swirly.com>
2024-08-15 13:44:47 +0000

    Several changes
    Modify python/test_simple_torch_function.py
    Add python/test_simple_torch_function2.py, python/test_simple_torch_function_tensor.py

--- python/test_simple_torch_function2.py
+++ python/test_simple_torch_function2.py
@@ -8,8 +8,9 @@ def simple_function(x):
 
 
 class TensorProxy(torch.Tensor):
-    pass
-
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        return super().__torch_function__(func, types, args, kwargs)
 
 torch._dynamo.config.traceable_tensor_subclasses.add(TensorProxy)
 proxy = torch.randn(1).as_subclass(TensorProxy)
--- python/test_simple_torch_function_tensor.py
+++ python/test_simple_torch_function_tensor.py
@@ -8,9 +8,10 @@ def simple_function(x):
 
 
 class TensorProxy(torch.Tensor):
-    pass
-
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        return super().__torch_function__(func, types, args, kwargs)
 
 torch._dynamo.config.traceable_tensor_subclasses.add(TensorProxy)
-proxy = torch.randn(1).as_subclass(TensorProxy)
+proxy = torch.randn(1) # .as_subclass(TensorProxy)
 simple_function(proxy)
