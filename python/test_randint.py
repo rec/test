db86500f2151af48ab0a59220b20ef00c8da4a6b
Tom Ritchford <tom@swirly.com>
2024-08-26 18:51:12 +0000

    Modify python/test_randint.py

--- python/test_randint.py
+++ python/test_randint.py
@@ -4,7 +4,8 @@ import random
 
 @torch._dynamo.optimize(nopython=True)
 def simple_function(rand2):
-    return rand2.randint(1, 9)
+    return random.randint(1, 9)
+    # return rand2.randint(1, 9)
 
 
 simple_function(random.Random(12))
