759e09279c7a76406d16f71aa6cc1d564cb882d2
Tom Ritchford <tom@swirly.com>
2024-09-02 17:16:18 +0000

    Modify python/clean_dynamo.py

--- python/clean_dynamo.py
+++ python/clean_dynamo.py
@@ -1,22 +1,23 @@
-import os
 import re
 
 M0 = re.compile(r'[A-Z]\d+').match
 M1 = re.compile(r'\d\d:\d\d:\d\d\.\d+').match
 M2 = re.compile(r'\d+').match
 
+def convert(line):
+    s = line.split(maxsplit=3)
+    if len(s) > 3 and M0(s[0]) and M1(s[1]) and M2(s[2]):
+        return f"{s[0]} {s[3]}"
+    else:
+        return line
+
 
 def convert_filename(fn):
-    for line in open(fn):
-        s = line.split(maxsplit=3)
-        if len(s) > 3 and M0(s[0]) and M1(s[1]) and M2(s[2]):
-            print(s[0], s[3])
-        else:
-            pass
-            # print(line)
+    lines = [convert(i) for i in open(fn)]
+    open(fn, 'w').writelines(lines)
 
 
 if __name__ == '__main__':
-    import sys
-
-    convert_filename(os.path.expanduser(sys.argv[1]))
+    import os, sys
+    for i in sys.argv[1:]:
+        convert_filename(os.path.expanduser(i))
