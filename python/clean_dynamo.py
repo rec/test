936626809092a1a9fb755a3cef28eee0f13dc22a
Tom Ritchford <tom@swirly.com>
2024-09-10 12:07:09 +0000

    Modify python/clean_dynamo.py

--- python/clean_dynamo.py
+++ python/clean_dynamo.py
@@ -7,29 +7,31 @@ M2 = re.compile(r'\d+').match
 HEX = re.compile(r'\b(0x[0-9a-f]+)\b')
 
 
-def convert(line, cache):
+def _remove_timestamps(line):
     s = line.split(maxsplit=3)
     if len(s) > 3 and M0(s[0]) and M1(s[1]) and M2(s[2]):
-        line = f"{s[0]} {s[3]}"
+        return f"{s[0]} {s[3]}"
+    return line
 
-    parts = HEX.split(line)
-    for i in range(1, len(parts), 2):
-        h = parts[i]
-        assert HEX.match(h)
-        try:
-            parts[i] = cache[h]
-        except KeyError:
-            hname = f'0x{len(cache):012x}'
-            parts[i] = hname
-            cache[h] = hname
 
-    return ''.join(parts)
+def _canonicalize_hex(line, cache):
+    for i, parts in enumerate(HEX.split(line)):
+        if i % 2:
+            assert HEX.match(h)
+            yield cache.setdefault(h, f'0x{len(cache):012x}')
+        else:
+            yield part
+
+
+def _convert(line, cache):
+    line = _remove_timestamps(line)
+    return ''.join(_canonicalize_hex(line, cache))
 
 
 def convert_filename(fn):
     cache = {}
-    lines = [convert(i, cache) for i in open(fn)]
-    open(fn, 'w').writelines(lines)
+    lines = list(open(fn))
+    open(fn, 'w').writelines(_convert(i, cache) for i in open(fn))
 
 
 if __name__ == '__main__':
