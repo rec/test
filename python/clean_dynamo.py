5a74a6d15c7e661b7675f38fb7de2291f0317870
Tom Ritchford <tom@swirly.com>
2024-09-10 11:39:02 +0000

    Several changes
    Modify python/clean_dynamo.py
    Add python/safer_bug.py

--- python/clean_dynamo.py
+++ python/clean_dynamo.py
@@ -4,16 +4,31 @@ M0 = re.compile(r'[A-Z]\d+').match
 M1 = re.compile(r'\d\d:\d\d:\d\d\.\d+').match
 M2 = re.compile(r'\d+').match
 
-def convert(line):
+HEX = re.compile(r'\b(0x[0-9a-f]+)\b')
+
+
+def convert(line, cache):
     s = line.split(maxsplit=3)
     if len(s) > 3 and M0(s[0]) and M1(s[1]) and M2(s[2]):
-        return f"{s[0]} {s[3]}"
-    else:
-        return line
+        line = f"{s[0]} {s[3]}"
+
+    parts = HEX.split(line)
+    for i in range(1, len(parts), 2):
+        h = parts[i]
+        assert HEX.match(h)
+        try:
+            parts[i] = cache[h]
+        except KeyError:
+            hname = f'0x{len(cache):012x}'
+            parts[i] = hname
+            cache[h] = hname
+
+    return ''.join(parts)
 
 
 def convert_filename(fn):
-    lines = [convert(i) for i in open(fn)]
+    cache = {}
+    lines = [convert(i, cache) for i in open(fn)]
     open(fn, 'w').writelines(lines)
 
 
