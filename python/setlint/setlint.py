7d5c5d34227bdb66e78eab93823353042d129f2b
Tom Ritchford <tom@swirly.com>
2024-10-02 14:19:48 +0000

    Modify python/setlint/setlint.py

--- python/setlint/setlint.py
+++ python/setlint/setlint.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import token
 from tokenize import tokenize, TokenInfo
 from typing import Iterator, Sequence, Tuple
@@ -6,6 +7,7 @@ from typing import Iterator, Sequence, Tuple
 _HELP_APPLY_PATCHES = 'Actually fix any issues, if possible'
 _HELP_EXCLUDE = 'Files or directories to exclude'
 _HELP_INCLUDE = 'Files or directories to include'
+_HELP_FILES = 'Files to check. Overrides --exclude and --include if set'
 
 TOKEN_TYPES = token.NAME, token.STRING, token.OP
 
@@ -14,7 +16,7 @@ Tokens = Sequence[TokenInfo]
 OMIT_COMMENT = '# noqa: setlint'
 
 
-def get_tokens(filename: str) -> Iterator[Tokens]:
+def get_all_tokens(filename: str) -> Iterator[Tokens]:
     with open(filename, 'rb') as fp:
         buffer = []
         for t in tokenize(fp.readline):
@@ -48,22 +50,51 @@ def omitted_lines(filename: str) -> Sequence[int]:
 def all_sets(filenames: Sequence[str]) -> Iterator[Tuple[str, TokenInfo]]:
     for filename in filenames:
         omitted = set(omitted_lines(filename))
-        for token_list in get_tokens(filename):
-            for token in get_set_tokens(token_list):
-                if not omitted.intersection([token.start[0], token.end[0]]):
-                    yield filename, token
+        for token_list in get_all_tokens(filename):
+            for tok in get_set_tokens(token_list):
+                if not omitted.intersection([tok.start[0], tok.end[0]]):
+                    yield filename, tok
 
 
-def parse_args() -> argparse.Namespace:
+def add_configs(args: argparse.Namespace, config_filename: str) -> argparse.Namespace:
+    try:
+        with open(filename) as fp:
+            config = json.load(fp)
+    except FileNotFoundError:
+        config = {}
+
+    if bad := set(config) - set(vars(args)):
+        s = '' if len(bad) == 1 else 's'
+        bad = ', '.join(sorted(bad))
+        raise ValueError(f'Unknown arg{s}: {bad}')
+
+    for k, v in config.items():
+        if k == 'apply_patches' and args.apply_patches is None:
+            args.apply_patches = v
+        else:
+            setattr(args, k, getattr(args, k) or v)
+
+
+def get_files(args: argparse.Namespace) -> Sequence[str]:
+    if args.files:
+        return files
+    raise NotImplementedError
+
+
+def make_parser() -> argparse.ArgumentParser:
     class HelpFormatter(argparse.HelpFormatter):
         def __init__(self, prog, indent_increment=4, max_help_position=16, width=None):
             super().__init__(prog, indent_increment, max_help_position, width)
 
     parser = argparse.ArgumentParser(formatter_class=HelpFormatter)
     add = parser.add_argument
-    add('files', nargs='*', help=_HELP_BRANCHES)
+    add('files', nargs='*', help=_HELP_FILES)
     add('-a', '--apply-patches', default=None, action='store_true', help=_HELP_APPLY_PATCHES)
     add('-e', '--exclude', action='append', help=_HELP_EXCLUDE)
     add('-i', '--include', action='append', help=_HELP_INCLUDE)
 
-    return parser.parse_args()
+
+def get_args(config_filename: str) -> argparse.args:
+    args = parser.parse_args()
+    add_configs(args, config_filename)
+    return args
