aef2003a3208ad469e0903399068baa448105771
Tom Ritchford <tom@swirly.com>
2024-10-02 16:20:51 +0000

    Several changes
    Modify python/setlint/test_setlint.py
    Remove python/setlint/setlint.py

--- python/setlint/test_setlint.py
+++ python/setlint/test_setlint.py
@@ -1,8 +1,8 @@
-import setlint
+import records
 import token
 
-TESTFILE = 'setlint-sample.txt'
-TESTFILE2 = 'setlint-sample-2.txt'
+TESTFILE = records.FileRecord('setlint-sample.txt')
+TESTFILE2 = records.FileRecord('setlint-sample-2.txt')
 
 EXPECTED_TOKENS = [
     [('NAME', 'a'), ('OP', '='), ('NAME', 'set'), ('OP', '('), ('OP', ')')],
@@ -15,38 +15,32 @@ EXPECTED_TOKENS = [
 
 EXPECTED_SETS = [
     (
-        TESTFILE,
         "TokenInfo(type=1 (NAME), string='set', start=(1, 4), end=(1, 7), "
-        "line='a = set()\\n')",
+        "line='a = set()\\n')"
     ),
     (
-        TESTFILE,
         "TokenInfo(type=1 (NAME), string='set', start=(3, 4), end=(3, 7), "
-        "line='c = set\\n')",
+        "line='c = set\\n')"
     ),
     (
-        TESTFILE,
         "TokenInfo(type=1 (NAME), string='set', start=(6, 3), end=(6, 6), "
-        "line='   set(\\n')",
+        "line='   set(\\n')"
     ),
 ]
 
 
 EXPECTED_SETS2 = [
     (
-        TESTFILE2,
         "TokenInfo(type=1 (NAME), string='set', start=(2, 4), end=(2, 7), "
-        "line='a = set()\\n')",
+        "line='a = set()\\n')"
     ),
     (
-        TESTFILE2,
         "TokenInfo(type=1 (NAME), string='set', start=(4, 4), end=(4, 7), "
-        "line='c = set\\n')",
+        "line='c = set\\n')"
     ),
     (
-        TESTFILE2,
         "TokenInfo(type=1 (NAME), string='set', start=(8, 3), end=(8, 6), "
-        "line='   set(\\n')",
+        "line='   set(\\n')"
     ),
 ]
 
@@ -55,25 +49,24 @@ def test_get_all_tokens():
     def _pair(t):
         return token.tok_name[t.type], t.string
 
-
-    tokens = setlint.get_all_tokens(TESTFILE)
+    tokens = TESTFILE.token_lines()
     actual = [[_pair(t) for t in tl] for tl in tokens]
     assert actual == EXPECTED_TOKENS
 
 
 def test_all_sets():
-    all_sets = list(setlint.all_sets([TESTFILE]))
-    actual = [(s, str(t)) for s, t in all_sets]
+    all_sets = list(TESTFILE.all_sets)
+    actual = [ft for ft in all_sets]
     assert actual == EXPECTED_SETS
 
 
 def test_omitted_lines():
-    actual = sorted(setlint.omitted_lines(TESTFILE2))
+    actual = sorted(TESTFILE2.omitted_lines)
     expected = [1, 5, 12]
     assert actual == expected
 
 
 def test_all_sets_omitted():
-    all_sets = list(setlint.all_sets([TESTFILE2]))
+    all_sets = list(TESTFILE2.all_sets())
     actual = [(s, str(t)) for s, t in all_sets]
     assert actual == EXPECTED_SETS2
