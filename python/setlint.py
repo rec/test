026eb3202dc1047bc7ce8a119ceb0ed6a9b608ca
Tom Ritchford <tom@swirly.com>
2024-10-02 12:13:43 +0000

    Modify python/setlint.py, python/test_setlint.py

--- python/setlint.py
+++ python/setlint.py
@@ -1,28 +1,49 @@
 import argparse
-import tokenize as tok
-import typing as t
+import token
+from tokenize import tokenize, TokenInfo
+from typing import Iterator, Sequence, Tuple
 
 _HELP_APPLY_PATCHES = 'Actually fix any issues, if possible'
 _HELP_EXCLUDE = 'Files or directories to exclude'
 _HELP_INCLUDE = 'Files or directories to include'
 
-TOKEN_TYPES = tok.NAME, tok.STRING, tok.OP
+TOKEN_TYPES = token.NAME, token.STRING, token.OP
+
+Tokens = Sequence[TokenInfo]
 
 
-def get_tokens(filename):
+def get_tokens(filename: str) -> Iterator[Tokens]:
     with open(filename, 'rb') as fp:
         buffer = []
-        for t in tok.tokenize(fp.readline):
+        for t in tokenize(fp.readline):
             if t.type in TOKEN_TYPES:
                 buffer.append(t)
-            elif t.type == tok.NEWLINE:
+            elif t.type == token.NEWLINE:
                 yield buffer
                 buffer = []
         if buffer:
             yield buffer
 
 
-def parse_args():
+def get_sets(tl: Tokens) -> Iterator[TokenInfo]:
+    def is_set(i, t):
+        return (
+            t.type == token.NAME
+            and t.string in ('set', 'set(')
+            and (not i or tl[i - 1].string != '.')
+        )
+
+    yield from (t for i, t in enumerate(tl) if is_set(i, t))
+
+
+def all_sets(filenames: Sequence[str]) -> Iterator[Tuple[str, TokenInfo]]:
+    for f in filenames:
+        for tl in get_tokens(f):
+            for s in get_sets(tl):
+                yield f, s
+
+
+def parse_args() -> argparse.Namespace:
     class HelpFormatter(argparse.HelpFormatter):
         def __init__(self, prog, indent_increment=4, max_help_position=16, width=None):
             super().__init__(prog, indent_increment, max_help_position, width)
@@ -30,7 +51,7 @@ def parse_args():
     parser = argparse.ArgumentParser(formatter_class=HelpFormatter)
     add = parser.add_argument
     add('files', nargs='*', help=_HELP_BRANCHES)
-    add('-a', '--apply-patches', action='store_true', help=_HELP_APPLY_PATCHES)
+    add('-a', '--apply-patches', default=None, action='store_true', help=_HELP_APPLY_PATCHES)
     add('-e', '--exclude', action='append', help=_HELP_EXCLUDE)
     add('-i', '--include', action='append', help=_HELP_INCLUDE)
 
--- python/test_setlint.py
+++ python/test_setlint.py
@@ -1,6 +1,7 @@
-from setlint import get_tokens, tok
+import setlint
+import token
 
-EXPECTED = [
+EXPECTED_TOKENS = [
     [('NAME', 'a'), ('OP', '='), ('NAME', 'set'), ('OP', '('), ('OP', ')')],
     [('NAME', 'b'), ('OP', '='), ('STRING', "'set()'")],
     [('NAME', 'c'), ('OP', '='), ('NAME', 'set')],
@@ -9,8 +10,37 @@ EXPECTED = [
     [('NAME', 'e'), ('OP', '='), ('STRING', '""" set()\nset() set x.set set()\n\\""""')],
 ]
 
+EXPECTED_SETS = [
+[
+    (
+        'setlint-sample.txt',
+        "TokenInfo(type=1 (NAME), string='set', start=(1, 4), end=(1, 7), "
+        "line='a = set()\\n')",
+    ),
+    (
+        'setlint-sample.txt',
+        "TokenInfo(type=1 (NAME), string='set', start=(3, 4), end=(3, 7), "
+        "line='c = set\\n')",
+    ),
+    (
+        'setlint-sample.txt',
+        "TokenInfo(type=1 (NAME), string='set', start=(6, 3), end=(6, 6), "
+        "line='   set(\\n')",
+    ),
+]
 
-def test_setlint():
-    tokens = get_tokens('setlint-sample.txt')
-    actual = [[(tok.tok_name[i.type], i.string) for i in t] for t in tokens]
-    assert actual == EXPECTED
+
+def test_get_tokens():
+    def _pair(t):
+        return token.tok_name[t.type], t.string
+
+
+    tokens = setlint.get_tokens('setlint-sample.txt')
+    actual = [[_pair(t) for t in tl] for tl in tokens]
+    assert actual == EXPECTED_TOKENS
+
+
+def test_all_sets():
+    all_sets = list(setlint.all_sets(['setlint-sample.txt']))
+    actual = [(s, str(t)) for s, t in all_sets]
+    assert actual == EXPECTED_SETS
