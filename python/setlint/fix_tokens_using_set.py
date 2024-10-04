47f74ef4fc1c325abe108015c5c1c7db096f51e8
Tom Ritchford <tom@swirly.com>
2024-10-04 17:14:37 +0200

    Add python/setlint/fix_tokens_using_set.py

---
+++ python/setlint/fix_tokens_using_set.py
@@ -0,0 +1,42 @@
+from tokenize import TokenInfo
+from typing import List
+
+
+def fix_tokens_using_set(filename: str, tokens: List[TokenInfo]) -> None:
+    if not tokens:
+        return
+
+    # First replace all the instances of set with OrderedSet
+    with open(filename) as fp:
+        lines = fp.readlines()
+
+    for t in tokens.sorted(key=lambda t: t.first, reverse=True):
+        start_line, start_col = t.start
+        end_line, end_col = t.end
+        assert start_line == end_line
+        line = lines[start_line]
+        assert line[start_col:end_col] == 'set'
+        pre, post = line[:start_col], line[end_col:]
+
+        lines[start_line] = f'{pre}OrderedSet{post}'
+
+    if not any(_match_import(line) for line in lines):
+        # Add the missing import and hope that ruff puts it in the right place
+        _add_import(lines)
+
+    with open(filename, 'w') as fp:
+        fp.writelines(lines)
+
+
+def _match_import(line: str) -> bool:
+    p = [j for i in line.split() for j in i.split('.') if j]
+    return (
+        bool(p)
+        and p[0] == 'from'
+        and p[-1] == 'OrderedSet'
+        and 'import' in p and 'utils' in p
+    )
+
+
+def _add_import(lines):
+    pass
