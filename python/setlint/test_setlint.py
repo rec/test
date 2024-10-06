2fcf47a12ced5e48442da46e4d3194cc8053d07c
Tom Ritchford <tom@swirly.com>
2024-10-06 18:41:14 +0200

    Many changes

--- python/setlint/test_setlint.py
+++ python/setlint/test_setlint.py
@@ -1,11 +1,47 @@
 from tokens_using_set import TokensUsingSet
 from omitted_lines import OmittedLines
 import token
+from tokenize import TokenInfo
 
 TESTFILE = TokensUsingSet("setlint-sample.txt")
-TESTFILE2 = TokensUsingSet("setlint-sample-2.txt")
+TESTFILE_OMITTED = TokensUsingSet("setlint-sample-omitted.txt")
 
-EXPECTED_TOKENS = [
+
+def test_token_lines():
+    def _pair(t):
+        return token.tok_name[t.type], t.string
+
+    actual = [[_pair(t) for t in tl.tokens] for tl in TESTFILE.token_lines]
+    assert EXPECTED_LINES == actual
+
+
+def test_get_all_tokens():
+    assert EXPECTED_SETS == TESTFILE.tokens
+
+
+def test_omitted_lines():
+    actual = sorted(OmittedLines(TESTFILE_OMITTED.filename).lines)
+    expected = [1, 5, 12]
+    assert expected == actual
+
+
+def test_all_sets_omitted():
+    assert TESTFILE_OMITTED.tokens == EXPECTED_SETS_OMITTED
+
+
+EXPECTED_SETS = [
+    TokenInfo(type=token.NAME, string='set', start=(1, 4), end=(1, 7), line='a = set()\n'),
+    TokenInfo(type=token.NAME, string='set', start=(3, 4), end=(3, 7), line='c = set\n'),
+    TokenInfo(type=token.NAME, string='set', start=(6, 3), end=(6, 6), line='   set(\n'),
+]
+
+EXPECTED_SETS_OMITTED = [
+    TokenInfo(type=token.NAME, string='set', start=(2, 4), end=(2, 7), line='a = set()\n'),
+    TokenInfo(type=token.NAME, string='set', start=(4, 4), end=(4, 7), line='c = set\n'),
+    TokenInfo(type=token.NAME, string='set', start=(8, 3), end=(8, 6), line='   set(\n'),
+]
+
+EXPECTED_LINES = [
     [("NAME", "a"), ("OP", "="), ("NAME", "set"), ("OP", "("), ("OP", ")")],
     [("NAME", "b"), ("OP", "="), ("STRING", "'set()'")],
     [("NAME", "c"), ("OP", "="), ("NAME", "set")],
@@ -45,59 +81,6 @@ EXPECTED_TOKENS = [
         ("OP", "."),
         ("NAME", "set"),
     ],
+    [('NAME', 'good'), ('OP', '='), ('OP', '{'), ('OP', '}')],
+    [('NAME', 'bad'), ('OP', '='), ('OP', '{'), ('OP', '}')],
 ]
-
-
-EXPECTED_SETS = [
-    (
-        "TokenInfo(type=1 (NAME), string='set', start=(1, 4), end=(1, 7), "
-        "line='a = set()\\n')"
-    ),
-    (
-        "TokenInfo(type=1 (NAME), string='set', start=(3, 4), end=(3, 7), "
-        "line='c = set\\n')"
-    ),
-    (
-        "TokenInfo(type=1 (NAME), string='set', start=(6, 3), end=(6, 6), "
-        "line='   set(\\n')"
-    ),
-]
-
-
-EXPECTED_SETS2 = [
-    (
-        "TokenInfo(type=1 (NAME), string='set', start=(2, 4), end=(2, 7), "
-        "line='a = set()\\n')"
-    ),
-    (
-        "TokenInfo(type=1 (NAME), string='set', start=(4, 4), end=(4, 7), "
-        "line='c = set\\n')"
-    ),
-    (
-        "TokenInfo(type=1 (NAME), string='set', start=(8, 3), end=(8, 6), "
-        "line='   set(\\n')"
-    ),
-]
-
-
-def test_get_all_tokens():
-    def _pair(t):
-        return token.tok_name[t.type], t.string
-
-    actual = [[_pair(t) for t in tl.tokens] for tl in TESTFILE.token_lines]
-    assert actual == EXPECTED_TOKENS
-
-    actual = [str(t) for t in TESTFILE.tokens]
-    assert len(actual) == len(EXPECTED_SETS)
-    assert actual == EXPECTED_SETS
-
-
-def test_omitted_lines():
-    actual = sorted(OmittedLines(TESTFILE2.filename).lines)
-    expected = [1, 5, 12]
-    assert actual == expected
-
-
-def test_all_sets_omitted():
-    actual = [str(i) for i in TESTFILE2.tokens]
-    assert actual == EXPECTED_SETS2
