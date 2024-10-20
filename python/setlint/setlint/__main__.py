50fbe0fa1a6a701d3b47da7796e6e1f7f466977a
Tom Ritchford <tom@swirly.com>
2024-10-20 22:21:28 +0200

    Added new lint_file.py

--- python/setlint/setlint/__main__.py
+++ python/setlint/setlint/__main__.py
@@ -1,7 +1,7 @@
 import sys
 from .files import resolve_python_files
-from .python_file import PythonFile
-from . import configs, fix_set_tokens
+from .lint_file import lint_file
+from . import configs
 
 CONFIG_FILE = "setlint.json"
 
@@ -11,24 +11,8 @@ def main():
     if not (python_files := resolve_python_files(args.files, args.exclude)):
         sys.exit("No files selected")
 
-    def pr(*a, **ka) -> None:
-        if args.verbose:
-            print(*a, file=sys.stderr, **ka)
-
     for f in python_files:
-        args.verbose and print(f, "Reading", file=sys.stderr)
-        pf = PythonFile(f)
-        if not pf.set_tokens:
-            pr(f, "OK")
-            continue
-        if not args.fix or args.verbose:
-            for t in pf.set_tokens():
-                pass
-        if args.fix:
-            lines, count = fix_set_tokens(pf)
-            with open(f, "w") as fp:
-                fp.writelines(lines)
-            print(f, f"fixed {count} error{'s' * (count != 1)}", file=sys.stderr)
+        lint_file(f)
 
 
 if __name__ == "__main__":
---
+++ python/setlint/setlint/lint_file.py
@@ -0,0 +1,53 @@
+from .python_file import PythonFile
+from .fix_set_tokens import fix_set_tokens
+from argparse import Namespace
+
+WINDOW = 5
+BEFORE = 2
+AFTER = WINDOW - BEFORE - 1
+ERROR = "Builtin `set` is deprecated"
+
+
+def lint_file(filename: str, args: Namespace) -> None:
+    def pr(*a, **ka) -> None:
+        if args.verbose:
+            print(*a, **ka)
+
+    def print_source(i, text):
+        if text is None:
+            i = ""
+            text = ""
+        print(f"{i:5} | {text}")
+
+    pr(filename, "Reading")
+    pf = PythonFile(filename)
+    if not pf.set_tokens:
+        pr(filename, "OK")
+        return
+
+    if args.fix:
+        count = fix_set_tokens(pf)
+        with open(filename, "w") as fp:
+            fp.writelines(pf.lines)
+        print(filename, f"fixed {count} error{'s' * (count != 1)}")
+        if not args.verbose:
+            return
+
+    padded = [None] * BEFORE + pf.lines + [None] * AFTER
+    padded_line = list(enumerate(padded))
+    for t in pf.set_tokens:
+        (line, start), (line2, end) = t.start, t.end
+        assert line == line2
+        assert start + 3 == end
+        window = padded_line[line - 1 : line - 1 + WINDOW]
+        before, after = window[:BEFORE], window[BEFORE:]
+
+        print(f"{filename}:{line}:{start}: {ERROR}")
+
+        for i, text in before:
+            print_source(i + line - BEFORE, text)
+
+        print_source("", " " * start + "^^^\n")
+
+        for line, text in after:
+            print_source(i + line - BEFORE, text)
