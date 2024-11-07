6e372155b0c47e7c31d91be5821cead11cf9eb30
Tom Ritchford <tom@swirly.com>
2024-11-07 13:57:09 +0000

    Modify python/time_access.py

--- python/time_access.py
+++ python/time_access.py
@@ -1,12 +1,17 @@
+from torch.utils._ordered_set import OrderedSet
 from timeit import timeit
 
 
 def time_in(container, item):
-    return '{:7.2f}'.format(1000 * timeit(lambda: item in container))
+    return '{:6.2f}'.format(1000 * timeit(number=100_000, stmt=lambda: item in container))
 
 
-for i in (0, 4, 16, 64, 256, 1024, 4096, 16384, 65536):
-    s = {str(i) for j in range(i)}
+print('|     i  |       set       |      tuple      |    OrderedSet   |')
+print('|        |  hit   |  miss  |   hit  |  miss  |   hit  |  miss  |')
+
+for i in (0, 1, 2, 3, 4, 5, 6, 7, 8, 16, 64, 256):
+    s = set(str(j) for j in range(i))
+    os = OrderedSet(str(j) for j in range(i))
     t = tuple(s)
     missing = str(-1)
 
@@ -14,12 +19,12 @@ for i in (0, 4, 16, 64, 256, 1024, 4096, 16384, 65536):
         middle = t[i // 2]
         hit_t = time_in(t, middle)
         hit_s = time_in(s, middle)
+        hit_os = time_in(os, middle)
     else:
-        hit_t = hit_s = 7 * ' '
+        hit_t = hit_s = hit_os = 6 * ' '
 
     miss_s = time_in(s, missing)
     miss_t = time_in(t, missing)
+    miss_os = time_in(os, missing)
 
-    print('|    i    |      set       |     tuple      |')
-    print('|         |  hit  |  miss  |  hit  |  miss  |')
-    print(f'| {i:7} | {hit_s} | {miss_s} | {hit_t} {miss_t}')
+    print(f'|  {i:4}  | {hit_s} | {miss_s} | {hit_t} | {miss_t} | {hit_os} | {miss_os} |')
