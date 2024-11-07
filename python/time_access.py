7f35649d8fe316565ee2fada05ca933ff2a1d7fb
Tom Ritchford <tom@swirly.com>
2024-11-07 13:58:37 +0000

    Modify python/time_access.py

--- python/time_access.py
+++ python/time_access.py
@@ -6,7 +6,7 @@ def time_in(container, item):
     return '{:6.2f}'.format(1000 * timeit(number=100_000, stmt=lambda: item in container))
 
 
-print('|     i  |       set       |      tuple      |    OrderedSet   |')
+print('|     i  |       set       |    OrderedSet   |      tuple      |')
 print('|        |  hit   |  miss  |   hit  |  miss  |   hit  |  miss  |')
 
 for i in (0, 1, 2, 3, 4, 5, 6, 7, 8, 16, 64, 256):
@@ -17,14 +17,14 @@ for i in (0, 1, 2, 3, 4, 5, 6, 7, 8, 16, 64, 256):
 
     if i:
         middle = t[i // 2]
-        hit_t = time_in(t, middle)
         hit_s = time_in(s, middle)
         hit_os = time_in(os, middle)
+        hit_t = time_in(t, middle)
     else:
-        hit_t = hit_s = hit_os = 6 * ' '
+        hit_s = hit_os = hit_t = 6 * ' '
 
     miss_s = time_in(s, missing)
-    miss_t = time_in(t, missing)
     miss_os = time_in(os, missing)
+    miss_t = time_in(t, missing)
 
-    print(f'|  {i:4}  | {hit_s} | {miss_s} | {hit_t} | {miss_t} | {hit_os} | {miss_os} |')
+    print(f'|  {i:4}  | {hit_s} | {miss_s} | {hit_os} | {miss_os} | {hit_t} | {miss_t} |')
