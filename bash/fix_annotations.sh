b1839a6e6fb1e636f602c4f3930a186e5a13db41
Tom Ritchford <tom@swirly.com>
2024-12-13 20:16:14 +0000

    Modify bash/fix_annotations.sh

--- bash/fix_annotations.sh
+++ bash/fix_annotations.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Upgrades a project using lintrunner to use Python 3.9 style annotations
+
 python ~/code/test/python/add_annotations.py $@
 pyupgrade --py39-plus $@
 ruff check --select TCH --unsafe-fixes --fix $@
+lintrunner -a --take=RUFF
+lintrunner -a --take=RUFF
