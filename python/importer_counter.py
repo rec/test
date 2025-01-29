fa8170ee8d80c8bddbcf807ac3fce2a7b9523072
Tom Ritchford <tom@swirly.com>
2025-01-29 17:27:46 +0000

    Modify python/importer_counter.py

--- python/importer_counter.py
+++ python/importer_counter.py
@@ -45,63 +45,10 @@ def list_imports(tokens) -> Iterator[str]:
             yield join_lines(tl)
 
 
-def imports(token_lines) -> Iterator[str]:
-    for tl in token_lines:
-        if (t := tl[0]).type != token.NAME:
-            pass
-        elif t.string == "from":
-            yield from _from(tl[1:])
-        elif t.string == "import":
-            yield from _import(tl[1:])
-
-
-def _from(tokens: list[TokenInfo]):
-    stack = []
-    for i, t in enumerate(tokens):
-        if t.type == token.NAME:
-            if t.string == "import":
-                break
-            else:
-                stack.append(t.string)
-
-        elif t.type == token.OP:
-            assert t.string == "."
-            stack.append(t.string)
-
-    base = "".join(reversed(stack))
-    yield from (f"{base}.{i}" for i in _import(tokens[i + 1::]))
-
-
-def _import(tokens: list[TokenInfo]):
-    stack = []
-    for t in tokens:
-        if t.type == token.NAME:
-            stack.append(t.string)
-        elif t.type != token.OP:
-            continue
-        elif t.string == ".":
-            stack.append(t.string)
-        elif t.string == ",":
-            assert stack
-            yield "".join(reversed(stack))
-            stack = []
-        else:
-            assert False
-
-    if stack:
-        yield "".join(reversed(stack))
-
-
 if __name__ == '__main__':
     for i in sys.argv[1:]:
         print(f"{i}:")
         with open(i) as fp:
             tokens = list(generate_tokens(fp.readline))
-            if False:
-                tl = token_lines(tokens)
-                print('Token count', len(tokens), 'tl', len(tl))
-                for line in imports(tl):
-                    print(line)
-            else:
-                for i in list_imports(tokens):
-                    print(i)
+            for i in list_imports(tokens):
+                print(i)
