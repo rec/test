fe5af65e14d3cfac7696e2caaf1dec8dd581f40e
Tom Ritchford <tom@swirly.com>
2025-01-29 19:13:12 +0000

    Modify python/importer_counter.py

--- python/importer_counter.py
+++ python/importer_counter.py
@@ -1,18 +1,8 @@
 import os
 import sys, token
+from pathlib import Path
 from tokenize import generate_tokens, TokenInfo
 from typing import Iterator
-
-"""
-"import" module ["as" identifier] ("," module ["as" identifier])*
-
-"from" relative_module "import" identifier ["as" identifier] ("," identifier ["as" identifier])*
-
-"from" relative_module "import" "(" identifier ["as" identifier] ("," identifier ["as" identifier])* [","] ")"
-
-"from" relative_module "import" "*"
-"""
-
 
 def token_lines(tokens) -> list[list[TokenInfo]]:
     """Returns lists of TokenInfo segmented by token.NEWLINE"""
@@ -33,9 +23,9 @@ def join_lines(tokens):
     lines = []
     for t in tokens:
         if t.start[0] != lineno:
-            lines.append(t.line.partition("#")[0])
+            lines.append(t.line.strip())
             lineno = t.start[0]
-    return " ".join("".join(lines).split()).strip()
+    return " ".join(" ".join(lines).split()).strip()
 
 
 def list_imports(tokens) -> Iterator[str]:
@@ -46,9 +36,12 @@ def list_imports(tokens) -> Iterator[str]:
 
 
 def split_import(line):
+    if "*" in line:
+        return []
+
     fr, _, im = line.partition('import')
     im = im.replace("(", "").replace(")", "")
-    parts = [p.split()[0] for p in im.split(",")]
+    parts = [ps.split()[0] for p in im.split(",") if (ps := p.strip())]
 
     if not fr:
         return parts
@@ -57,11 +50,29 @@ def split_import(line):
     return [f"{rest}.{p}" for p in parts]
 
 
+def all_python_files(path: Path, python_root=None):
+    python_root = python_root or Path(".")
+    if path.suffix == ".py":
+        paths = [path]
+    else:
+        paths  = sorted(path.glob("**/*.py"))
+    for f in paths:
+        f = f.relative_to(python_root)
+        module_path = [i.name for i in reversed(f.parents)][1:]
+        if any('.' in i for i in module_path):
+            continue
+
+        with f.open() as fp:
+            tokens = list(generate_tokens(fp.readline))
+
+        for imp in list_imports(tokens):
+            for i in split_import(imp):
+                simp = i.lstrip(".")
+                if diff := len(i) - len(simp):
+                    i = ".".join(module_path[:(1 - diff) or None] + [simp])
+                print(f, i)
+
+
 if __name__ == '__main__':
     for i in sys.argv[1:]:
-        print(f"{i}:")
-        with open(i) as fp:
-            tokens = list(generate_tokens(fp.readline))
-            for i in list_imports(tokens):
-                for ip in split_import(i):
-                    print(ip)
+        all_python_files(Path(i))
