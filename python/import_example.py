b04a32e660550ac0b75d9b7a274e5dfe8a3b5f7e
Tom Ritchford <tom@swirly.com>
2025-01-30 14:32:56 +0000

    Modify python/import_example.py, python/importer_counter.py

--- python/import_example.py
+++ python/import_example.py
@@ -1,10 +1,21 @@
 from one.two import three, four, five
 
+
 import first, second.third
 
+
 from six import (
+
+
+
     seven, eight, nine
+
+
 )
+
+
+
+
 
 from ten.eleven import twelve as zero, five as thirteen
 
--- python/importer_counter.py
+++ python/importer_counter.py
@@ -1,3 +1,4 @@
+import itertools
 import json
 import sys
 import token
@@ -5,56 +6,32 @@ from pathlib import Path
 from tokenize import generate_tokens, TokenInfo
 from typing import Iterator
 
-def token_lines(tokens) -> list[list[TokenInfo]]:
-    token_lines: list[list[TokenInfo]] = [[]]
 
-    for t in tokens:
-        if t.type not in (token.COMMENT, token.ENDMARKER, token.NL):
-            token_lines[-1].append(t)
-            if t.type == token.NEWLINE:
-                token_lines.append([])
-    while token_lines and not token_lines[-1]:
-        token_lines.pop()
-    return token_lines
-
-
-def join_lines(tokens):
-    lineno = 0
-    lines = []
-    for t in tokens:
-        if t.start[0] != lineno:
-            lines.append(t.line.strip())
-            lineno = t.start[0]
-    return " ".join(" ".join(lines).split()).strip()
-
-
-def list_imports(tokens) -> Iterator[str]:
-    for tl in token_lines(tokens):
-        t = tl[0]
-        if t.type == token.NAME and t.string in ("from", "import"):
-            yield join_lines(tl)
+def import_lines(tokens) -> Iterator[str]:
+    it = [t for t in tokens if t.type not in (token.COMMENT, token.NL)]
+    for _, i in itertools.groupby(it, lambda t: t.type == token.NEWLINE):
+        token_list = list(i)
+        t = token_list and token_list[0]
+        if t and t.type == token.NAME and t.string in ("from", "import"):
+            lines = {j.start[0]: j.line for j in token_list}
+            yield " ".join(" ".join(lines.values()).split()).strip()
 
 
 def split_import(line):
     if "*" in line:
         return []
 
-    fr, _, im = line.partition('import')
-    im = im.replace("(", "").replace(")", "")
-    parts = [ps.split()[0] for p in im.split(",") if (ps := p.strip())]
+    before, _, after = line.partition('import')
+    assert after, line
+    after = after.replace("(", "").replace(")", "").strip()
+    parts = [ps.split()[0] for p in after.split(",") if (ps := p.strip())]
 
-    if not fr:
-        return parts
-    fr, _, rest = fr.strip().partition("from ")
-    assert not fr
-    return [f"{rest}.{p}" for p in parts]
+    if before:
+        before, _, imp = before.strip().partition("from ")
+        assert not before, f"{before=}, {line[:120]=}"
+        parts = [f"{imp}.{p}" for p in parts]
 
-
-def bucket(it):
-    result = {}
-    for k, v in it:
-        result.setdefault(k, []).append(v)
-    return result
+    return parts
 
 
 def all_python_files(path: Path, python_root=None):
@@ -71,6 +48,12 @@ def all_python_files(path: Path, python_root=None):
     else:
         path_prefix = str(path).strip("/").replace("/", ".") + "."
 
+    def bucket(it):
+        result = {}
+        for k, v in it:
+            result.setdefault(k, []).append(v)
+        return result
+
     result = bucket((mp, i) for f in paths for mp, i in one_file(f))
     result = {k: [i for i in v if i.startswith(path_prefix)] for k, v in result.items()}
     inverse = bucket((i, k) for k, v in result.items() for i in v)
@@ -81,17 +64,18 @@ def one_file(f):
     module_path = [i.name for i in reversed(f.parents)]
     if any('.' in i for i in module_path):
         return
-    mp = ".".join(module_path)
+    mp = ".".join(module_path + [f.name])
 
     with f.open() as fp:
         tokens = list(generate_tokens(fp.readline))
 
-    for imp in list_imports(tokens):
-        for i in split_import(imp):
-            simp = i.lstrip(".")
-            if diff := len(i) - len(simp):
-                i = ".".join(module_path[1:(1 - diff) or None] + [simp])
-            yield mp, i
+    print(list(import_lines(tokens)))
+    for line in import_lines(tokens):
+        for imp in split_import(line):
+            simp = imp.lstrip(".")
+            if diff := len(imp) - len(simp):
+                imp = ".".join(module_path[1:(1 - diff) or None] + [simp])
+            yield mp, imp
 
 
 if __name__ == '__main__':
