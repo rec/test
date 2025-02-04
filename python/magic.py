b789da4ca91eefc18f4d0060739a5790d088ba21
Tom Ritchford <tom@swirly.com>
2025-02-04 15:16:51 +0000

    Modify python/magic.py

--- python/magic.py
+++ python/magic.py
@@ -3,37 +3,33 @@ from itertools import product
 
 def write_file():
     print(IMPORTS)
-    print()
-    print()
-
     for t in TYPES:
         print(f"def accept_{t.lower()}(x: {t}) -> None:\n    pass\n")
 
-    for t in TYPES:
-        print(f"{t[0].lower()}: {t} = {TYPES[t]}")
+    if False:
+        for t in TYPES:
+            print(f"{t[0].lower()}: {t} = {TYPES[t]}")
 
     types = [t.lower() for t in TYPES]
 
     print("\n#\n# Test unary ops\n#")
-    for ftype in types:
-        print(f"\n# {ftype=}")
-        for rtype in types:
-            print(f"\n# {rtype=}")
-            for op in UNARY:
-                if len(op) == 1:
-                    print(f"accept_{ftype}({op}{rtype[0]})")
-                else:
-                    print(f"accept_{ftype}({op}({rtype[0]}))")
+    for atype in types:
+        for op in UNARY:
+            if len(op) == 1:
+                print(f"accept_{atype}({op}{TENSOR})")
+            else:
+                print(f"accept_{atype}({op}({TENSOR}))")
 
     print("\n#\n# Test binary ops\n#")
-    for ftype in types:
-        print(f"\n# {ftype=}")
-        for ltype in types:
-            print(f"\n# {ltype=}")
-            for rtype in types:
-                print(f"\n# {rtype=}")
+    for atype in types:
+        for type, v in TYPES.items():
+            for op in BINARY_OPS:
+                print(f"accept_{atype}({TENSOR} {op} {v})")
+
+            if TENSOR != v:
+                print()
                 for op in BINARY_OPS:
-                    print(f"accept_{ftype}({ltype[0]} {op} {rtype[0]})")
+                    print(f"accept_{atype}({v} {op} {TENSOR})")
 
 
 LOGICAL = {
@@ -74,21 +70,22 @@ BINARY = {
 
 BINARY_OPS = *LOGICAL, *BINARY, *(f"{op}=" for op in BINARY)
 
-
 TYPES = {
     'bool': "True",
-    'int': "23",
-    'Tensor': 'torch.randn(3, 3)',
-    'float': "3.25",
-    'str': "'xyzzy'",
+    'int': "2",
+    'Tensor': "randn(3)",
+    'float': "1.5",
+    'str': "'s'",
 }
+TENSOR = TYPES["Tensor"]
 
-IMPORTS = """
+IMPORTS = """\
 import math
 
-import torch
-from torch import Tensor
-""".strip()
+from torch import randn, Tensor
+
+
+"""
 
 
 if __name__ == "__main__":
