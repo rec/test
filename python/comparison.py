df16ca1a0a508e89a3cff7663e6225e41b42e985
Tom Ritchford <tom@swirly.com>
2025-02-06 14:24:11 +0000

    Several changes
    Modify python/comparison.py
    Add python/magic-out.txt, python/magic.txt

--- python/comparison.py
+++ python/comparison.py
@@ -1,31 +1,28 @@
+class Binary:
+    def __eq__(self, other: int) -> "Binary":  # type: ignore[override]
+        print("__eq__", other)
+        return self
+
+    def __ne__(self, other: int) -> "Binary":  # type: ignore[override]
+        print("__ne__", other)
+        return self
+
+    def __lt__(self, other: int) -> "Binary":  # type: ignore[override]
+        print("__lt__", other)
+        return self
+
+    def __le__(self, other: int) -> "Binary":  # type: ignore[override]
+        print("__le__", other)
+        return self
+
+
+# from typing import assert_type
 from typing_extensions import assert_type
 
+assert_type(Binary() < 1, Binary)
+assert_type(Binary() == 2, Binary)
+assert_type(Binary() != 3, Binary)
 
-class Test:
-    def __eq__(self, other: int) -> "Test":  # type: ignore[override]
-        print("__eq__", self, other)
-        return self
-
-    def __ne__(self, other: int) -> "Test":  # type: ignore[override]
-        print("__ne__", self, other)
-        return self
-
-    def __lt__(self, other: int) -> "Test":  # type: ignore[override]
-        print("__lt__", self, other)
-        return self
-
-    def __gt__(self, other: int) -> "Test":  # type: ignore[override]
-        print("__gt__", self, other)
-        return self
-
-    def __le__(self, other: int) -> "Test":  # type: ignore[override]
-        print("__le__", self, other)
-        return self
-
-    def __ge__(self, other: int) -> "Test":  # type: ignore[override]
-        print("__ge__", self, other)
-        return self
-
-
-assert_type(Test() < 1, Test)
-assert_type(1 >= Test(), Test)
+assert_type(4 >= Binary(), bool)
+assert_type(5 == Binary(), bool)
+assert_type(6 != Binary(), bool)
