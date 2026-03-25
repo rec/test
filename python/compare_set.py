a5485b8564d7a4b9588f43dd9197342a3977d04f
Tom Ritchford <tom@swirly.com>
2026-03-25 11:37:59 +0100

    Modify python/compare_set.py

--- python/compare_set.py
+++ python/compare_set.py
@@ -1,11 +1,11 @@
 filter_list = [f"{i:03}" for i in range(1000)]
 hints = {"291": (1, 1), "245": (1, 0), "463": (2, 0), "578": (0, 0), "569": (1, 0)}
 
-for k, (correct, right_place) in hints.items():
-    def position_set(value: str) -> set[str]:
-        return {f'{k}{i}' for i, k in enumerate(value)}
+for number, (correct, right_place) in hints.items():
+    def position_set(s: str) -> set[str]:
+        return {f'{k}{i}' for i, k in enumerate(s)}
 
-    sk, psk = set(k), position_set(k)
+    sk, psk = set(number), position_set(number)
 
     def matches_hint(n: str) -> bool:
         return len(sk & set(n)) == correct and len(psk & position_set(n)) == right_place
