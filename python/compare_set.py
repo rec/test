29093284f5803b8b29c7768ee58b8ff70046c3eb
Tom Ritchford <tom@swirly.com>
2026-03-25 11:08:19 +0100

    Add python/compare_set.py

---
+++ python/compare_set.py
@@ -0,0 +1,12 @@
+def positional_set(value):
+    return {f'{k}{i}' for i, k in enumerate(value)}
+
+filter_list = [f"{i:03}" for i in range(1000)]
+hints = {"291": (1, 1), "245": (1, 0), "463": (2, 0), "578": (0, 0), "569": (1, 0)}
+for (k, v) in hints.items():
+    filter_list = list(filter(
+        lambda hint: len(set(k).intersection(set(hint))) == v[0] and
+                     len(set(positional_set(k)).intersection(set(positional_set(hint)))) == v[1],
+        filter_list
+    ))
+print(filter_list)  # Output: ['394']
