47dbcb165cf46120f1d5e6e66ad1b43856022021
Tom Ritchford <tom@swirly.com>
2026-03-25 11:19:10 +0100

    Modify python/compare_set.py

--- python/compare_set.py
+++ python/compare_set.py
@@ -4,10 +4,7 @@ def positional_set(value):
 filter_list = [f"{i:03}" for i in range(1000)]
 hints = {"291": (1, 1), "245": (1, 0), "463": (2, 0), "578": (0, 0), "569": (1, 0)}
 for (k, v) in hints.items():
-    filter_list = list(filter(
-        lambda hint: len(set(k).intersection(set(hint))) == v[0] and
-                     len(set(positional_set(k)).intersection(set(positional_set(hint)))) == v[1],
-        filter_list
-    ))
+    filter_list = [hint for hint in filter_list if len(set(k).intersection(set(hint))) == v[0] and
+                     len(set(positional_set(k)).intersection(set(positional_set(hint)))) == v[1]]
 assert filter_list == ['394'], filter_list
 print('ok')
