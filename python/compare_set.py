724f2feefb717b28c23f333b27404f589b00db67
Tom Ritchford <tom@swirly.com>
2026-03-25 12:25:19 +0100

    Modify python/compare_set.py

--- python/compare_set.py
+++ python/compare_set.py
@@ -1,11 +1,11 @@
 hints = {"291": (1, 1), "245": (1, 0), "463": (2, 0), "578": (0, 0), "569": (1, 0)}
-
 filter_list = [
     number for i in range(1000)
-    if
-        all(len(set(number := f'{i:03}') & set(n)) == correct
+    if all(
+        len(set(number := f'{i:03}') & set(n)) == correct
         and sum(i == j for i, j in zip(number, n)) == right_place
-        for n, (correct, right_place) in hints.items())
+        for n, (correct, right_place) in hints.items()
+    )
 ]
 
 assert filter_list == ['394'], filter_list
